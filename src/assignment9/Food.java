8e6506cbeddad8ef1d6168b630e36302386882ab
lance <lance@LanceComputer2.dhcp.wustl.edu>
2025-04-23 11:53:12 -0500

    DONE FOREVER YAY!

--- src/assignment9/Food.java
+++ src/assignment9/Food.java
@@ -1,26 +1,29 @@
 package assignment9;
 
 import java.awt.Color;
-
+import java.util.Random;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Food {
+    public static final double FOOD_SIZE = 0.02;
+    private double x, y;
 
-	public static final double FOOD_SIZE = 0.02;
-	private double x, y;
-	
-	/**
-	 * Creates a new Food at a random location
-	 */
-	public Food() {
-		//FIXME
-	}
-	
-	/**
-	 * Draws the Food
-	 */
-	public void draw() {
-		//FIXME
-	}
-	
+    public Food() {
+        Random rand = new Random();
+        this.x = FOOD_SIZE + (1 - 2 * FOOD_SIZE) * rand.nextDouble();
+        this.y = FOOD_SIZE + (1 - 2 * FOOD_SIZE) * rand.nextDouble();
+    }
+
+    public void draw() {
+        StdDraw.setPenColor(Color.RED);
+        StdDraw.filledCircle(x, y, FOOD_SIZE);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
--- src/assignment9/Snake.java
+++ src/assignment9/Snake.java
@@ -3,66 +3,72 @@ package assignment9;
 import java.util.LinkedList;
 
 public class Snake {
+    private static final double SEGMENT_SIZE = 0.02;
+    private static final double MOVEMENT_SIZE = SEGMENT_SIZE * 1.5;
+    private LinkedList<BodySegment> segments;
+    private double deltaX;
+    private double deltaY;
+    private boolean shouldGrow = false;
 
-	private static final double SEGMENT_SIZE = 0.02;
-	private static final double MOVEMENT_SIZE = SEGMENT_SIZE * 1.5;
-	private LinkedList<BodySegment> segments;
-	private double deltaX;
-	private double deltaY;
-	
-	public Snake() {
-		//FIXME - set up the segments instance variable
-		deltaX = 0;
-		deltaY = 0;
-	}
-	
-	public void changeDirection(int direction) {
-		if(direction == 1) { //up
-			deltaY = MOVEMENT_SIZE;
-			deltaX = 0;
-		} else if (direction == 2) { //down
-			deltaY = -MOVEMENT_SIZE;
-			deltaX = 0;
-		} else if (direction == 3) { //left
-			deltaY = 0;
-			deltaX = -MOVEMENT_SIZE;
-		} else if (direction == 4) { //right
-			deltaY = 0;
-			deltaX = MOVEMENT_SIZE;
-		}
-	}
-	
-	/**
-	 * Moves the snake by updating the position of each of the segments
-	 * based on the current direction of travel
-	 */
-	public void move() {
-		//FIXME
-	}
-	
-	/**
-	 * Draws the snake by drawing each segment
-	 */
-	public void draw() {
-		//FIXME
-	}
-	
-	/**
-	 * The snake attempts to eat the given food, growing if it does so successfully
-	 * @param f the food to be eaten
-	 * @return true if the snake successfully ate the food
-	 */
-	public boolean eatFood(Food f) {
-		//FIXME
-		return false;
-	}
-	
-	/**
-	 * Returns true if the head of the snake is in bounds
-	 * @return whether or not the head is in the bounds of the window
-	 */
-	public boolean isInbounds() {
-		//FIXME
-		return true;
-	}
+    public Snake() {
+        segments = new LinkedList<>();
+        segments.add(new BodySegment(0.5, 0.5, SEGMENT_SIZE));
+        deltaX = 0;
+        deltaY = 0;
+    }
+
+    public void changeDirection(int direction) {
+        if (direction == 1) {
+            deltaY = MOVEMENT_SIZE;
+            deltaX = 0;
+        } else if (direction == 2) {
+            deltaY = -MOVEMENT_SIZE;
+            deltaX = 0;
+        } else if (direction == 3) {
+            deltaY = 0;
+            deltaX = -MOVEMENT_SIZE;
+        } else if (direction == 4) {
+            deltaY = 0;
+            deltaX = MOVEMENT_SIZE;
+        }
+    }
+
+    public void move() {
+        BodySegment head = segments.getFirst();
+        double newX = head.getX() + deltaX;
+        double newY = head.getY() + deltaY;
+        segments.addFirst(new BodySegment(newX, newY, SEGMENT_SIZE));
+
+        if (!shouldGrow) {
+            segments.removeLast();
+        } else {
+            shouldGrow = false;
+        }
+    }
+
+    public void draw() {
+        for (BodySegment segment : segments) {
+            segment.draw();
+        }
+    }
+
+    public boolean eat(Food f) {
+        BodySegment head = segments.getFirst();
+        double dx = head.getX() - f.getX();
+        double dy = head.getY() - f.getY();
+        double distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < SEGMENT_SIZE + Food.FOOD_SIZE) {
+            shouldGrow = true;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isInbounds() {
+        BodySegment head = segments.getFirst();
+        double x = head.getX();
+        double y = head.getY();
+        return x >= 0 && x <= 1 && y >= 0 && y <= 1;
+    }
 }
